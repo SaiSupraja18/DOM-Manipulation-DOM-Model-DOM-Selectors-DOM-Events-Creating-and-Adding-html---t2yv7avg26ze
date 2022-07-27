6a887c8edd7d206e6bab09d2ee25dadf49ba0287
SaiSupraja <102989692+SaiSupraja18@users.noreply.github.com>
2022-07-27 15:56:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -157,4 +157,4 @@ function createSuccessMessage(email, phone){
                               <p>Email: ${email}</p>
                               <p>Phone number: ${phone}</p>`;
      booker.appendChild(successElement);
-}
+} 
